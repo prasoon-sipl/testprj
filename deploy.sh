614757a47c5f34df5292276acde4cae26d5e18a5
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2017-09-12 18:22:15 +0530

    testrun4

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 for BRANCH in `git branch --list|sed 's/\*//g'`;
   do 
     echo "$BRANCH"
-    aws s3 website s3://{$Branch}/ --index-document index.html --error-document error.html
+    aws s3 website s3://$Branch/ --index-document index.html --error-document error.html
   done
