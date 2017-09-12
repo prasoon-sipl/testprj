38c29b58daf205bd7779062231048d9d378e02bd
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2017-09-12 19:01:01 +0530

    testrun8

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ for BRANCH in `git branch --list|sed 's/\*//g'`;
     echo "$BRANCH"
     aws s3 mb s3://$BRANCH.systematixinfotech.com
     aws s3 website s3://$BRANCH.systematixinfotech.com/ --index-document index.html --error-document error.html
-    aws s3 sync /home/ubuntu/testprj s3://$BRANCH --delete
+    aws s3 sync /home/ubuntu/testprj s3://$BRANCH.systematixinfotech.com --delete
   done
