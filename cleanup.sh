01eed6e0a53fb8d6bcbbf51d12d8d5e196c5d324
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2017-09-13 16:58:01 +0530

    trialcomm1

--- cleanup.sh
+++ cleanup.sh
@@ -5,8 +5,11 @@
 #    echo "$BRANCH"
 #    echo $CIRCLE_BRANCH
 
-for names in `aws s3api list-buckets --query "Buckets[].Name"`;
+for bucketlist in `aws s3api list-buckets --query "Buckets[].Name"`;
 	do
-	 echo $names;
+	 for bucketname in "${bucketlist[@]}"
+		do
+			echo "$bucketname"
+		done
 	done
 #  done
