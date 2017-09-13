2cafac340e50d7e34aa3372fa3ace79e4af24ff3
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2017-09-13 18:41:53 +0530

    commit

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-#for BRANCH in `git branch --list|sed 's/\*//g'`;
-#  do 
-#    echo "$BRANCH"
-#    echo $CIRCLE_BRANCH
-
-    aws s3 mb s3://$CIRCLE_BRANCH.systematixinfotech.com
-    aws s3 website s3://$CIRCLE_BRANCH.systematixinfotech.com/ --index-document index.html --error-document error.html
-    aws s3 sync /home/ubuntu/testprj s3://$CIRCLE_BRANCH.systematixinfotech.com --delete
-#  done
+    if [ $CIRCLE_BRANCH = 'master' ]; 
+		then aws s3 sync /home/ubuntu/testprj s3://master.systematixinfotech.com --delete;
+	else
+	    aws s3 mb s3://$CIRCLE_BRANCH.systematixinfotech.com
+        aws s3 website s3://$CIRCLE_BRANCH.systematixinfotech.com/ --index-document index.html --error-document error.html
+        aws s3 sync /home/ubuntu/testprj s3://$CIRCLE_BRANCH.systematixinfotech.com --delete
+    fi
+   
