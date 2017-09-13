6c0ada4b9a1f0a233d00c10010a1b4ddb3536bb0
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2017-09-13 14:50:23 +0530

    try

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@
 #  do 
 #    echo "$BRANCH"
 #    echo $CIRCLE_BRANCH
+    while read OLDSHA NEWSHA REF ; do
+    if [ "$NEWSHA" = "0000000000000000000000000000000000000000" ]; then
+    echo "# This ref has been deleted! Respond appropriately."
+    fi
     aws s3 mb s3://$CIRCLE_BRANCH.systematixinfotech.com
     aws s3 website s3://$CIRCLE_BRANCH.systematixinfotech.com/ --index-document index.html --error-document error.html
     aws s3 sync /home/ubuntu/testprj s3://$CIRCLE_BRANCH.systematixinfotech.com --delete
