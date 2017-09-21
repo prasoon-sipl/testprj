c9acbda22dcae3d66de1979bced2cdfcd244c332
rahulgaikwad <rahulgaikwad>
2017-09-21 17:51:45 +0530

    cleanup test5

--- cleanup.sh
+++ cleanup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-
-s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[][]//g' -e 's/"//g' -e 's/,//g' -e 's/.systematixinfotech.com//g' -e '/^$/d' -e 's/^[ \t]*//;s/[ \t]*$//')
 bucket_list=()
+s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[][]//g' -e 's/"//g' -e 's/,//g' -e 's/.systematixinfotech.com//g' -e '/^$/d' -e 's/^[ \t]*//;s/[ \t]*$//')
+
 for bucket in $(echo "$s3_bucket_list")
 do
-   bucket_list +=($bucket)
+   bucket_list+=("$bucket")
+  
 done
 
-echo "${bucket_list[*]}"
+echo ${bucket_list[@]}   
+exit
