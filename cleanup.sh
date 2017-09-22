0e84ccddf6f41246c9a880bdc11b7256b2b320b4
rahulgaikwad <rahulgaikwad>
2017-09-22 17:30:56 +0530

    clean up test

--- cleanup.sh
+++ cleanup.sh
@@ -40,7 +40,7 @@ different_bucket_name=(`echo ${bucket_list1[@]} ${branch_list[@]} | tr ' ' '\n' 
 # ===Delete unmatched bucket by name ==========================
 for j in "${different_bucket_name[@]}"
 do
-    #echo "$j" 
+    echo "$j" 
 	a="$j"
 	b=".systematixinfotech.com"
 
