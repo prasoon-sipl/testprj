4a8960c583594dbdefd97db3ce54eb50dd9366db
rahulgaikwad <rahulgaikwad>
2017-09-22 11:29:09 +0530

    Clenup test

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 bucket_list=()
-bucket_list1=()
 branch_list=()
+branch_list1=()
 different_bucket_name=()
 
 s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[][]//g' -e 's/"//g' -e 's/,//g' -e '/^$/d' -e 's/^[ \t]*//;s/[ \t]*$//')
@@ -13,25 +13,22 @@ do
   match=$(echo "$bucket" | grep -o $search_string)  
   [[ ! -z $match ]] && bucket_list+=("$bucket")
 done
-#==========================
-# search_string=".systematixinfotech.com"  
-# match=$(echo "${bucket_list[@]:0}" | grep -o $search_string)  
-# [[ ! -z $match ]] && bucket_list1+=("$bucket")
-#=====================================================
+#=======================================================
 
-		# branchs=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+for i in "${bucket_list[@]}"
+do
+   b = sed -e 's/.systematixinfotech.com//g' "$i"
+   bucket_list1+=("b")
+done
 
-		# for branch_name in $(echo "$branchs") 
-		# do 
-		# 	branch_list+=("$branch_name")
-		# 	#echo $branch; 
-		# done
+#============================================
+
 for branch_name in `git branch --list|sed 's/\*//g'`;
  do 
    branch_list+=("$branch_name")
  done
 #========================================================
-echo ${bucket_list[@]} 
+echo ${bucket_list1[@]} 
 exit
 #echo ${branch_list[@]} 
 
