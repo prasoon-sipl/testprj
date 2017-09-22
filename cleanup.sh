3d819efa4b714f7fdc1053cee9e56c447573ae6c
Prasoon Sharma <prasoon.sharma@systematixindia.com>
2017-09-22 14:51:07 +0530

    wsdhwjdchwd

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#for BRANCH in `git branch --list|sed 's/\*//g'`;
-#  do 
-#    echo "$BRANCH"
-#    echo $CIRCLE_BRANCH
+for BRANCH in `git branch --list|sed 's/\*//g'`;
+  do 
+    echo "$BRANCH"
+    echo $CIRCLE_BRANCH
 
-for bucketlist in `aws s3api list-buckets --query "Buckets[].Name" | sed 's/.systematixinfotech\.*//'`;
-	do
-	 for bucketname in "${bucketlist[@]}"
-		do
-			echo "$bucketname"
-		done
-	done
-#  done
+#for bucketlist in `aws s3api list-buckets --query "Buckets[].Name" | sed 's/.systematixinfotech\.*//'`;
+#	do
+#	 for bucketname in "${bucketlist[@]}"
+#		do
+#			echo "$bucketname"
+#		done
+#	done
+  done
