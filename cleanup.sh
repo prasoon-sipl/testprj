263d8eba007be67c7c6c07f7a76a75aef911fcb7
rahulgaikwad <rahulgaikwad>
2017-09-22 11:02:10 +0530

    Clenup test

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 bucket_list=()
+bucket_list1=()
 branch_list=()
 different_bucket_name=()
 
-s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[][]//g' -e 's/"//g' -e 's/,//g' -n 's/.*\([.systematixinfotech.com]*G[.systematixinfotech.com]*\).*/\1/p'  -e 's/.systematixinfotech.com//g' -e '/^$/d' -e 's/^[ \t]*//;s/[ \t]*$//')
-
+s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[][]//g' -e 's/"//g' -e 's/,//g' -e '/^$/d' -e 's/^[ \t]*//;s/[ \t]*$//')
+#-e 's/.systematixinfotech.com//g'
 for bucket in $(echo "$s3_bucket_list")
 do
    bucket_list+=("$bucket")
   
 done
+#==========================
+search_string=".systematixinfotech.com"  
+match=$(echo "${bucket_list[@]:0}" | grep -o $search_string)  
+[[ ! -z $match ]] && bucket_list1+=("$bucket")
 #=====================================================
 
 		# branchs=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
@@ -24,7 +29,7 @@ for branch_name in `git branch --list|sed 's/\*//g'`;
    branch_list+=("$branch_name")
  done
 #========================================================
-echo ${bucket_list[@]} 
+echo ${bucket_list1[@]} 
 exit
 #echo ${branch_list[@]} 
 
