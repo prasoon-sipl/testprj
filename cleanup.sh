dda8b9d784d35441d55b4f208ea32afffc6f1dc2
rahulgaikwad <rahulgaikwad>
2017-09-27 11:55:11 +0530

    update remote prune

--- cleanup.sh
+++ cleanup.sh
@@ -4,6 +4,11 @@ branch_list=()
 branch_list1=()
 different_bucket_name=()
 
+`git remote prune origin`
+
+#==============================
+#  Fetch bucket list from S3  #
+#==============================
 s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[][]//g' -e 's/"//g' -e 's/,//g' -e '/^$/d' -e 's/^[ \t]*//;s/[ \t]*$//')
 
 for bucket in $(echo "$s3_bucket_list")
@@ -18,33 +23,33 @@ do
    b=${i//.systematixinfotech.com/}
    bucket_list1+=("$b")
 done
-#============================================
+
+#==============================
+#  Fetch Branch list from git #
+#==============================
 
 for remote in `git branch -r |grep -v HEAD | sed 's/\*//g'| sed "s/origin\///" `; 
 do 
     branch_list+=("$remote") 
 done
 
-
-#========================================================
-#echo ${bucket_list1[@]} 
-#echo ${branch_list[@]} 
+#=========================================
+#  Get bucket name for non exists branch  #
+#=========================================
 
 different_bucket_name=(`echo ${bucket_list1[@]} ${branch_list[@]} | tr ' ' '\n' | sort | uniq -u `)
 
-#echo ${different_bucket_name[@]} 
 
-#echo ${different_bucket_name[@]} 
-
-# ===Delete unmatched bucket by name ==========================
+#=================================
+# Delete unmatched bucket by name #
+#=================================
 for j in "${different_bucket_name[@]}"
 do
-  #echo "$j" 
+  
   bucket1="$j"
   bucket2=".systematixinfotech.com"
   bucket3="$bucket1$bucket2"
 
-  #echo "$bucket3"
   aws s3api delete-bucket --bucket $bucket3
 done
 
