1e72f0a229ca1b799283cfa619a60fb3ebf205fe
rahulgaikwad <rahulgaikwad>
2017-09-27 12:48:54 +0530

    update remote prune

--- cleanup.sh
+++ cleanup.sh
@@ -48,20 +48,22 @@ different_bucket_name=(`echo ${bucket_list1[@]} ${branch_list[@]} | tr ' ' '\n' 
 # Delete unmatched bucket by name    #
 # if bucket list is not empty .       # 
 #=====================================
-echo "${bucket_list1[*]}"
-if [ ${#bucket_list1[@]} -eq 0 ]; then
-    echo "No buckets for deletion."
-else
-    for j in "${different_bucket_name[@]}"
-    do
+
+echo ${bucket_list1[@]}
+
+# if [ ${#bucket_list1[@]} -eq 0 ]; then
+#     echo "No buckets for deletion."
+# else
+#     for j in "${different_bucket_name[@]}"
+#     do
       
-      bucket1="$j"
-      bucket2=".systematixinfotech.com"
-      bucket3="$bucket1$bucket2"
-      echo $bucket3
-      #aws s3api delete-bucket --bucket $bucket3
-    done
-fi
+#       bucket1="$j"
+#       bucket2=".systematixinfotech.com"
+#       bucket3="$bucket1$bucket2"
+#       echo $bucket3
+#       #aws s3api delete-bucket --bucket $bucket3
+#     done
+# fi
 
 
 
