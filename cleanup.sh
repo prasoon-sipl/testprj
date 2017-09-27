75bb99dbea08345b4d59d0878dcb95c40c98d93e
rahulgaikwad <rahulgaikwad>
2017-09-27 12:18:50 +0530

    update remote prune

--- cleanup.sh
+++ cleanup.sh
@@ -4,7 +4,11 @@ branch_list=()
 branch_list1=()
 different_bucket_name=()
 
+#==================================================================
+# Deleting the refs to the branches that don't exist on the remote #
+#==================================================================
 git remote prune origin
+
 
 #==============================
 #  Fetch bucket list from S3  #
@@ -14,7 +18,7 @@ s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[]
 for bucket in $(echo "$s3_bucket_list")
 do
   search_string=".systematixinfotech.com"  
-  match=$(echo "$bucket" | grep -o $search_string)  
+  match=$(echo "$bucket" | grep -o '$search_string')  
   [[ ! -z $match ]] && bucket_list+=("$bucket")
 done
 
@@ -40,16 +44,25 @@ done
 different_bucket_name=(`echo ${bucket_list1[@]} ${branch_list[@]} | tr ' ' '\n' | sort | uniq -u `)
 
 
-#=================================
-# Delete unmatched bucket by name #
-#=================================
-for j in "${different_bucket_name[@]}"
-do
-  
-  bucket1="$j"
-  bucket2=".systematixinfotech.com"
-  bucket3="$bucket1$bucket2"
-  echo $bucket3
-  #aws s3api delete-bucket --bucket $bucket3
-done
+#=====================================
+# Delete unmatched bucket by name    #
+# if bucket list is not empty        # 
+#=====================================
+
+if [ ${#errors[@]} -eq 0 ]; then
+    echo "No buckets for deletion."
+else
+    for j in "${different_bucket_name[@]}"
+    do
+      
+      bucket1="$j"
+      bucket2=".systematixinfotech.com"
+      bucket3="$bucket1$bucket2"
+      echo $bucket3
+      #aws s3api delete-bucket --bucket $bucket3
+    done
+fi
+
+
+
 
