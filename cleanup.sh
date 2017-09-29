766fcc763ec723b50cc34ed4848703167b9e9f4a
rahulgaikwad <rahulgaikwad>
2017-09-29 11:46:44 +0530

    my_branch

--- cleanup.sh
+++ cleanup.sh
@@ -17,14 +17,14 @@ s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[]
 
 for bucket in $(echo "$s3_bucket_list")
 do
-  search_string=".systematixinfotech.com"  
-  match=$(echo "$bucket" | grep -o '$search_string')  
+  search_string="-stagingdev.gardenuity.com"  
+  match=$(echo "$bucket" | grep -o $search_string)  
   [[ ! -z $match ]] && bucket_list+=("$bucket")
 done
 
 for i in "${bucket_list[@]}"
 do
-   b=${i//.systematixinfotech.com/}
+   b=${i//-stagingdev.gardenuity.com/}
    bucket_list1+=("$b")
 done
 
@@ -58,7 +58,7 @@ echo ${bucket_list1[@]}
 #     do
       
 #       bucket1="$j"
-#       bucket2=".systematixinfotech.com"
+#       bucket2="-stagingdev.gardenuity.com"
 #       bucket3="$bucket1$bucket2"
 #       echo $bucket3
 #       #==delete bucket=====================
