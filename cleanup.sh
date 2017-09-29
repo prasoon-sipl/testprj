0cb5024851a0b89e73650426f2def96651e91735
rahulgaikwad <rahulgaikwad>
2017-09-29 11:54:44 +0530

    my_branch

--- cleanup.sh
+++ cleanup.sh
@@ -18,7 +18,7 @@ s3_bucket_list=$(aws s3api list-buckets --query 'Buckets[*].Name' | sed -e 's/[]
 for bucket in $(echo "$s3_bucket_list")
 do
   search_string="-stagingdev.gardenuity.com"  
-  match=$(echo "$bucket" | grep -o $search_string)  
+  match=$(echo "$bucket" | grep -rn '$search_string')  #-o
   [[ ! -z $match ]] && bucket_list+=("$bucket")
 done
 
