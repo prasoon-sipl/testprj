4c549e31e03dcd032a13a2b5490656917cb6ec07
rahulgaikwad <rahulgaikwad>
2017-10-03 14:11:01 +0530

    my_branch

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
   
 
 function JSONizePolicy() {
-	echo $1
+	echo '{"Version":"2012-10-17","Id":"'"$1"'","Statement":[{"Sid":"PublicReadForGetBucketObjects","Effect":"Allow","Principal":"*","Action":["s3:GetObject","s3:PutObject"],"Resource":"arn:aws:s3:::'"$1"'/*"}]}'
+	#echo $1
 	#echo '{"Version": "2012-10-17","Statement": [{"Action": ["s3:GetObject","s3:PutObject"],"Effect": "Allow","Resource": ["arn:aws:s3:::'"$s3bucketname"'/*"]}]}'> policy.json
 	echo '{"Version":"2012-10-17","Id":"'"$1"'","Statement":[{"Sid":"PublicReadForGetBucketObjects","Effect":"Allow","Principal":"*","Action":["s3:GetObject","s3:PutObject"],"Resource":"arn:aws:s3:::'"$1"'/*"}]}' > policy.json
 }
@@ -17,9 +18,8 @@ if [ $CIRCLE_BRANCH = 'master' ];
 	    JSONizePolicy $CIRCLE_BRANCH.systematixinfotech.com
 
 	    aws s3api put-bucket-policy --bucket $CIRCLE_BRANCH.systematixinfotech.com --policy file://policy.json 2>&1
-        
-       # aws s3 website s3://$CIRCLE_BRANCH.systematixinfotech.com/ --index-document index.html --error-document error.html
-       aws s3 website $CIRCLE_BRANCH.systematixinfotech.com --index-document index.html
+       
+        aws s3 website s3://$CIRCLE_BRANCH.systematixinfotech.com/ --index-document index.html --error-document error.html
        
         aws s3 sync /home/ubuntu/testprj s3://$CIRCLE_BRANCH.systematixinfotech.com --delete
 fi
