9ba87f94ee0436d5442141862d9b0d0de75e8c13
rahulgaikwad <rahulgaikwad>
2017-10-03 14:04:49 +0530

    my_branch

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 function JSONizePolicy() {
 	echo $1
 	#echo '{"Version": "2012-10-17","Statement": [{"Action": ["s3:GetObject","s3:PutObject"],"Effect": "Allow","Resource": ["arn:aws:s3:::'"$s3bucketname"'/*"]}]}'> policy.json
-	echo '{"Version":"2012-10-17","Id":"'"$1"'","Statement":[{"Sid":"PublicReadForGetBucketObjects","Effect":"Allow","Principal":"*","Action":"s3:GetObject","Resource":"arn:aws:s3:::'"$1"'/*"}]}' > policy.json
+	echo '{"Version":"2012-10-17","Id":"'"$1"'","Statement":[{"Sid":"PublicReadForGetBucketObjects","Effect":"Allow","Principal":"*","Action":["s3:GetObject","s3:PutObject"],"Resource":"arn:aws:s3:::'"$1"'/*"}]}' > policy.json
 }
 
 if [ $CIRCLE_BRANCH = 'master' ]; 
