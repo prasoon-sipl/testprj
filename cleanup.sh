e8d30305a80056e2dc862a4a0b766484e8d455b2
rahulgaikwad <rahulgaikwad>
2017-10-04 12:15:44 +0530

    test-branch

--- cleanup.sh
+++ cleanup.sh
@@ -48,8 +48,8 @@ different_bucket_name=(`echo ${bucket_list1[@]} ${branch_list[@]} | tr ' ' '\n' 
 # Delete unmatched bucket by name    #
 # if bucket list is not empty .       # 
 #=====================================
-
-#echo ${different_bucket_name[@]}
+echo ${bucket_list1[@]}
+echo ${different_bucket_name[@]}
 
 if [ ${#bucket_list1[@]} -eq 0 ]; then
     echo "No buckets for deletion."
