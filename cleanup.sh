e6117d39c8dcf09178683bed43c97beeb9ff14c8
rahulgaikwad <rahulgaikwad>
2017-10-04 12:37:20 +0530

    test-branch

--- cleanup.sh
+++ cleanup.sh
@@ -49,7 +49,7 @@ different_bucket_name=(`echo ${bucket_list1[@]} ${branch_list[@]} | tr ' ' '\n' 
 # if bucket list is not empty .       # 
 #=====================================
 echo ${bucket_list1[@]}
-#echo ${branch_list[@]}
+echo ${branch_list[@]}
 echo ${different_bucket_name[@]}
 
 if [ ${#bucket_list1[@]} -eq 0 ]; then
@@ -62,14 +62,15 @@ else
       bucket2=".systematixinfotech.com"
       bucket3="$bucket1$bucket2"
       
-      echo $bucket3
+      #echo $bucket3
       if aws s3api head-bucket --bucket "$bucket3" 2>/dev/null; then
         
-        echo "found"
+       
         #==delete bucket=====================
-        #aws s3 rb s3://$bucket3 --force
-      else
-        echo "not found"
+        aws s3 rb s3://$bucket3 --force
+        echo "bucket found and deleted."
+      #else
+        #echo "not found"
       fi
 
     done
